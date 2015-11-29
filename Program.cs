6705f4c84ff6c7db63ccb3f6822b606fe0c2d2b4
Steven Brown <sjbrown8@mail.usf.edu>
2015-11-29 09:21:46 -0500

    Problem completed

--- Program.cs
+++ Program.cs
@@ -1,12 +1,19 @@
 using System;
+using System.Numerics;
+using System.Collections.Generic;
 
 namespace Console_13
-{
-	class MainClass
-	{
-		public static void Main (string[] args)
-		{
-			Console.WriteLine ("Hello World!");
+{	class MainClass
+	{	public static void Main (string[] args)
+		{	BigInteger total = 0;
+
+			for(int i = 0; i < 100; i++){
+				total += BigInteger.Add (DataClass.data [i, 2], 
+						 BigInteger.Add( BigInteger.Multiply(BigInteger.Pow(10, 19), (DataClass.data [i, 1])), 
+				               			 BigInteger.Multiply(BigInteger.Pow(10, 38), (DataClass.data [i, 0]))
+				                       ));
+			}
+			Console.WriteLine (total);
 		}
 	}
 }
